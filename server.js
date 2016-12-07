a143dc46b4ba509431695d768a20ddf3b1862a17
CruzLeyvaSegundo <segundoleyva@usp.br>
2016-12-07 15:13:11 -0200

    links redireccionados correctamente

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
-//  OpenShift sample Node application
-var http = require("http"),
-	//fs      = require('fs'),
-	express = require('express'),
+var express = require('express'),
     app     = express();
 
-//var html = fs.readFileSync("views/index.html");
 app.use(express.static('images'));
 app.use(express.static('css'));
 app.use(express.static('scripts'));
+
 app.engine('html', require('ejs').renderFile);
 app.set("view engine","html");
+
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 	
 app.get('/', function (req, res) {
     res.render('index', {});
-	//res.writeHead(200,{"Content-Type":"text/html"});
-	//res.write(html);
+});
+
+app.get('/cadastro', function (req, res) {
+    res.render('cadastro', {});
+});
+
+app.get('/produtos', function (req, res) {
+    res.render('productos', {});
+});
+
+app.get('/servicios', function (req, res) {
+    res.render('servicios', {});
 });
 
 
